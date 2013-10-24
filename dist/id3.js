6f3ce93c58b3a335f50520fb42e3a2ef592c640e
43081j <43081j@server.local>
2013-10-24 01:32:30 +0100

    ID3v2.2 and earlier

--- dist/id3.js
+++ dist/id3.js
@@ -356,9 +356,8 @@
 			return decodeURIComponent(escape(str));
 		};
 
-		DataView.prototype.getUint32Synch = function(offset) {
-			var num = this.getUint32(offset),
-				out = 0,
+		DataView.prototype.getSynch = function(num) {
+			var out = 0,
 				mask = 0x7f000000;
 			while(mask) {
 				out >>= 1;
@@ -366,6 +365,14 @@
 				mask >>= 8;
 			}
 			return out;
+		};
+
+		DataView.prototype.getUint32Synch = function(offset) {
+			return this.getSynch(this.getUint32(offset));
+		};
+
+		DataView.prototype.getUint24 = function(offset) {
+			return this.getUint8(offset + 2) + (this.getUint8(offset + 1) << 8) + (this.getUint8(offset) << 16);
 		};
 
 
@@ -429,6 +436,38 @@
 			'TSSE': 'encoder-settings',
 			'TSST': 'set-subtitle',
 			/*
+			 * Textual frames (<=2.2)
+			 */
+			'TAL': 'album',
+			'TBP': 'bpm',
+			'TCM': 'composer',
+			'TCO': 'genre',
+			'TCR': 'copyright',
+			'TDY': 'playlist-delay',
+			'TEN': 'encoder',
+			'TFT': 'file-type',
+			'TKE': 'initial-key',
+			'TLA': 'language',
+			'TLE': 'length',
+			'TMT': 'media-type',
+			'TOA': 'original-artist',
+			'TOF': 'original-filename',
+			'TOL': 'original-writer',
+			'TOT': 'original-album',
+			'TP1': 'artist',
+			'TP2': 'band',
+			'TP3': 'conductor',
+			'TP4': 'remixer',
+			'TPA': 'set-part',
+			'TPB': 'publisher',
+			'TRC': 'isrc',
+			'TRK': 'track',
+			'TSS': 'encoder-settings',
+			'TT1': 'content-group',
+			'TT2': 'title',
+			'TT3': 'subtitle',
+			'TXT': 'writer',
+			/*
 			 * URL frames
 			 */
 			'WCOM': 'url-commercial',
@@ -440,14 +479,51 @@
 			'WPAY': 'url-payment',
 			'WPUB': 'url-publisher',
 			/*
+			 * URL frames (<=2.2)
+			 */
+			'WAF': 'url-file',
+			'WAR': 'url-artist',
+			'WAS': 'url-source',
+			'WCM': 'url-commercial',
+			'WCP': 'url-copyright',
+			'WPB': 'url-publisher',
+			/*
 			 * Comment frame
 			 */
 			'COMM': 'comments',
 			/*
 			 * Image frame
 			 */
-			'APIC': 'image'
+			'APIC': 'image',
+			'PIC': 'image'
 		};
+
+		/*
+		 * ID3 image types
+		 */
+		ID3Frame.imageTypes = [
+			'other',
+			'file-icon',
+			'icon',
+			'cover-front',
+			'cover-back',
+			'leaflet',
+			'media',
+			'artist-lead',
+			'artist',
+			'conductor',
+			'band',
+			'composer',
+			'writer',
+			'location',
+			'during-recording',
+			'during-performance',
+			'screen',
+			'fish',
+			'illustration',
+			'logo-band',
+			'logo-publisher'
+		];
 
 		/*
 		 * ID3v2.3 and later
@@ -514,30 +590,7 @@
 						mime: null,
 						description: null,
 						data: null
-					},
-					imageTypes = [
-						'other',
-						'file-icon',
-						'icon',
-						'cover-front',
-						'cover-back',
-						'leaflet',
-						'media',
-						'artist-lead',
-						'artist',
-						'conductor',
-						'band',
-						'composer',
-						'writer',
-						'location',
-						'during-recording',
-						'during-performance',
-						'screen',
-						'fish',
-						'illustration',
-						'logo-band',
-						'logo-publisher'
-					];
+					};
 				var variableStart = 11, variableLength = 0;
 				for(var i = variableStart;;i++) {
 					if(dv.getUint8(i) === 0x00) {
@@ -546,7 +599,7 @@
 					}
 				}
 				image.mime = dv.getString(variableLength, variableStart);
-				image.type = imageTypes[dv.getUint8(variableStart + variableLength + 1)] || 'other';
+				image.type = ID3Frame.imageTypes[dv.getUint8(variableStart + variableLength + 1)] || 'other';
 				variableStart += variableLength + 2;
 				variableLength = 0;
 				for(var i = variableStart;; i++) {
@@ -566,6 +619,58 @@
 		 * ID3v2.2 and earlier
 		 */
 		ID3Frame.parseLegacy = function(buffer) {
+			var result = {tag: null, value: null},
+				dv = new DataView(buffer),
+				header = {
+					id: dv.getString(3),
+					type: dv.getString(1),
+					size: dv.getUint24(3)
+				};
+			if(!header.id in ID3Frame.types) {
+				return false;
+			}
+			result.tag = ID3Frame.types[header.id];
+			if(header.type === 'T') {
+				var encoding = dv.getUint8(7);
+				/*
+				 * TODO: Implement UTF-8, UTF-16 and UTF-16 with BOM properly?
+				 */
+				result.value = dv.getString(-7, 7);
+				if(header.id === 'TCO' && !!parseInt(result.value)) {
+					result.value = Genres[parseInt(result.value)];
+				}
+			} else if(header.type === 'W') {
+				result.value = dv.getString(-7, 7);
+			} else if(header.id === 'COM') {
+				/*
+				 * TODO: Implement UTF-8, UTF-16 and UTF-16 with BOM properly?
+				 */
+				var encoding = dv.getUint8(6);
+				result.value = dv.getString(-10, 10);
+				if(result.value.indexOf('\x00') !== -1) {
+					result.value = result.value.substr(result.value.indexOf('\x00') + 1);
+				}
+			} else if(header.id === 'PIC') {
+				var encoding = dv.getUint8(6),
+					image = {
+						type: null,
+						mime: 'image/' + dv.getString(3, 7).toLowerCase(),
+						description: null,
+						data: null
+					};
+				image.type = ID3Frame.imageTypes[dv.getUint8(11)] || 'other';
+				var variableStart = 11, variableLength = 0;
+				for(var i = variableStart;; i++) {
+					if(dv.getUint8(i) === 0x00) {
+						variableLength = i - variableStart;
+						break;
+					}
+				}
+				image.description = (variableLength === 0 ? null : dv.getString(variableLength, variableStart));
+				image.data = buffer.slice(variableStart + 1);
+				result.value = image;
+			}
+			return (result.tag ? result : false);
 		};
 
 		/*
@@ -708,8 +813,7 @@
 						 * >= v2.3, frame ID is 4 chars, size is 4 bytes, flags are 2 bytes, total 10 bytes
 						 */
 						if(tags.v2.version[0] < 3) {
-							var frameSize = ((dv.getUint8(position + 5) & 0xFF) << 8) | ((dv.getUint8(position + 4) & 0xFF) << 16) | ((dv.getUint8(position + 3) & 0xFF) << 24);
-							slice = buffer.slice(position, position + 6 + frameSize);
+							slice = buffer.slice(position, position + 6 + dv.getUint24(position + 3));
 						} else {
 							slice = buffer.slice(position, position + 10 + dv.getUint32Synch(position + 4));
 						}
